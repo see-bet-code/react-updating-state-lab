bbb6c1f413d99965678e64991236406fd338ed9b
see-bet-code <brittani.elan.taylor@gmail.com>
2020-11-19 22:48:59 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,22 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react';
+
+export default class DigitalClicker extends Component {
+  state = {
+    timesClicked: 0
+  }
+  
+  increaseTimesClicked = () => {
+    this.setState(previousState => {
+      return {
+        timesClicked: previousState.timesClicked + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <button onClick={this.increaseTimesClicked}>{this.state.timesClicked}</button>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,37 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+  state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: '1080p'
+      }
+    }
+  }
+
+  handleBitrateChange = () => {
+    this.setState(
+      {settings: {...this.state.settings, bitrate: 12}}
+      );
+  }
+
+  handleResolutionChange = () => {
+    this.setState( 
+      {settings: {...this.state.settings, video:
+        {resolution: '720p'}}}
+      );
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.handleBitrateChange}/>
+        <button className="resolution" onClick={this.handleResolutionChange}/>
+      </div> 
+    )
+  }
+}
